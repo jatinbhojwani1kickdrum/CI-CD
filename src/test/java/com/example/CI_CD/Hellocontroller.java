8ce1a594a55b428ece1c872b3200a618e47aaf17
jatinbhojwani1kickdrum <jatin.bhojwani@kickdrumtech.com>
2026-02-10 18:00:30 +0530

    added the code

---
+++ src/test/java/com/example/CI_CD/Hellocontroller.java
@@ -0,0 +1,19 @@
+package com.example.CI_CD;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellocontroller {
+
+    @GetMapping("/")
+    public String home() {
+        return "Deployed on Elastic Beanstalk ✅";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
